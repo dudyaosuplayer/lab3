4eabba9ab6328900b5d0d5d67b062cca5dd1ad54
dudyaosuplayer <62661898+dudyaosuplayer@users.noreply.github.com>
2020-04-24 12:23:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,33 +21,63 @@ case 1:
 {
 cout << "Input value of length : ";
 cin >> val_a;
+while (val_a < 0)
+{
+cout << "Input correct value of length : ";
+cin >> val_a;
+}
 cout << "Input value of width : ";
 cin >> val_b;
+while (val_b < 0)
+{
+cout << "Input correct value of width : ";
+cin >> val_b;
+}
 RecTangle obj1(val_a, val_b);
 obj1.print();
-cout << obj1.square() << "; " << obj1.perimeter() << endl;
+cout << "square = " << obj1.square() << "; perimeter = " << obj1.perimeter() << endl;
 break;
 }
 case 2:
 {
 cout << "Input value of radius : ";
 cin>>val_r;
+while (val_r < 0)
+{
+cout << "Input correct value of radius : ";
+cin >> val_r;
+}
 Circle obj2(val_r);
 obj2.print();
-cout << obj2.square() << "; " << obj2.perimeter() << endl;
+cout << "square = " << obj2.square() << "; perimeter = " << obj2.perimeter() << endl;
 break;
 }
 case 3:
 {
 cout << "Input value of first side : ";
 cin >> val_a;
+while (val_a < 0)
+{
+cout << "Input correct value of first side : ";
+cin >> val_a;
+}
 cout << "Input value of second side : ";
 cin >> val_b;
-cout << "Input third side : ";
+while (val_b < 0)
+{
+cout << "Input correct value of second side : ";
+cin >> val_b;
+}
+cout << "Input value of third side : ";
 cin >> val_c;
+while (val_c < 0)
+{
+cout << "Input correct value of third side : ";
+cin >> val_c;
+}
 Triangle obj3(val_a, val_b, val_c);
 obj3.print();
-cout << obj3.square() << "; " << obj3.perimeter() << endl;
+cout << "square = " << obj3.square() << "; perimeter = " << obj3.perimeter() << endl;
 break;
 }
 case 4:
